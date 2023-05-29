b52509831971a730ed6f4833d5595c75e9e6d362
smaspons <smaspons@bgeo.es>
2023-05-29 08:32:49 +0000

    Added GwLoadPlugin

---
+++ plugins/GwLoadPlugin.jsx
@@ -0,0 +1,87 @@
+/**
+ * Copyright BGEO. All rights reserved.
+ * The program is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version
+ */
+
+import axios from 'axios';
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import isEmpty from 'lodash.isempty';
+import GwUtils from '../utils/GwUtils';
+import { zoomToExtent } from 'qwc2/actions/map';
+import { refreshLayer, changeLayerProperty } from 'qwc2/actions/layers';
+
+
+class GwLoadPlugin extends React.Component {
+    static propTypes = {
+        currentTask: PropTypes.string,
+        layers: PropTypes.array,
+        map: PropTypes.object,
+        theme: PropTypes.object,
+    }
+
+    state = {
+        pendingRequests: false,
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.theme != this.props.theme) {
+            this.makeRequest(true)
+        }
+    }
+    componentDidMount() {
+        console.log("MOUNTED LOAD PLUGIn")
+    }
+
+    makeRequest(hide_form = false) {
+        let pendingRequests = false;
+
+        const request_url = GwUtils.getServiceUrl("util");
+        if (!isEmpty(request_url)) {
+            // Get request paramas
+            const epsg = this.crsStrToInt(this.props.map.projection)
+            const params = {
+                "theme": this.props.theme.title,
+                "epsg": epsg,
+            }
+            // Send request
+            pendingRequests = true
+
+               // Send request
+            axios.post(request_url + "setinitproject", { ...params }).then(response => {
+                const result = response.data
+                console.log("LOADED PLUGIN: ", result);
+                this.setState({pendingRequests: false });
+            }).catch((e) => {
+                console.log(e);
+                this.setState({ pendingRequests: false });
+            });
+        }
+        
+    }
+
+    crsStrToInt = (crs) => {
+        const parts = crs.split(':')
+        return parseInt(parts.slice(-1))
+    }
+    
+    render() {
+        return null
+    }
+}
+
+const loadplugin = (state) => ({
+    currentTask: state.task.id,
+    layers: state.layers.flat,
+    map: state.map,
+    theme: state.theme.current
+});
+
+export default connect(loadplugin, {
+    zoomToExtent: zoomToExtent,
+    refreshLayer: refreshLayer,
+    changeLayerProperty: changeLayerProperty
+})(GwLoadPlugin);
