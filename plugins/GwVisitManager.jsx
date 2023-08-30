60b8f68158ebba451a1937b052915ef9353ee15c
edgarfuste <34737125+edgarfuste@users.noreply.github.com>
2023-08-30 10:39:51 +0200

    Update headers files

--- plugins/GwVisitManager.jsx
+++ plugins/GwVisitManager.jsx
@@ -1,5 +1,5 @@
 /**
- * Copyright BGEO. All rights reserved.
+ * Copyright © 2023 by BGEO. All rights reserved.
  * The program is free software: you can redistribute it and/or modify it under the terms of the GNU
  * General Public License as published by the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version
