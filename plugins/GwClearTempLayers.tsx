79948632d4ff53fa38d93725d2df4efe41b7b88d
BgeoDevTeam <smaspons@bgeo.es>
2024-07-22 09:53:42 +0000

    refactor(ClearTempLayers): .jsx -> .tsx proof of concept

---
+++ plugins/GwClearTempLayers.tsx
@@ -0,0 +1,86 @@
+/**
+ * Copyright © 2023 by BGEO. All rights reserved.
+ * The program is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version
+ */
+
+import React from 'react';
+import { connect } from 'react-redux';
+import Icon from 'qwc2/components/Icon';
+import { removeLayer } from 'qwc2/actions/layers';
+import 'qwc2/plugins/style/Buttons.css';
+import { setCurrentTask } from 'qwc2/actions/task';
+import { LayerRole } from 'qwc2/actions/layers';
+
+type GwClearTempLayersButtonProps = {
+    closeTasks: boolean,
+    position: number,
+    layers: any[],
+    removeLayer: (layerId: any) => void,
+    setCurrentTask: (task: any) => void,
+    currentTask: string
+};
+
+class GwClearTempLayersButton extends React.Component<GwClearTempLayersButtonProps> {
+
+    redliningOpen: boolean;
+
+    static defaultProps = {
+        closeTasks: false
+    };
+
+    constructor(props) {
+        super(props);
+        this.redliningOpen = false;
+    }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.currentTask === "Redlining" && this.props.currentTask !== "Redlining" && this.redliningOpen) {
+            this.redliningOpen = false;
+            this.clearLayers();
+        }
+    }
+
+    clearLayers() {
+        this.props.layers.forEach(layer => {
+            if (layer.role === LayerRole.USERLAYER) {
+                this.props.removeLayer(layer.id);
+            }
+        });
+
+        if (this.props.closeTasks) {
+            this.props.setCurrentTask(null);
+        }
+    }
+    
+    btnClicked(){
+        if (this.props.currentTask === "Redlining"){
+            this.props.setCurrentTask(null);
+            this.redliningOpen = true
+        }
+        else{
+            this.clearLayers();
+        }
+    }
+
+    render() {
+        return (
+            <button className="map-button"
+                onClick={() => this.btnClicked()}
+                style={{ bottom: (5 + 4 * this.props.position) + 'em' }}
+                title="Clear Temporal Layers"
+            >
+                <Icon icon="trash" title="Clear Temporal Layers" />
+            </button>
+        );
+    }
+}
+
+export default connect(state => ({ // @ts-ignore
+    layers: state.layers.flat,  // @ts-ignore
+    currentTask: state.task.id
+}), {
+    removeLayer: removeLayer,
+    setCurrentTask: setCurrentTask
+})(GwClearTempLayersButton);
